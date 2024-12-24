4d2421171f24ca5a62bcdd4fcffc3f6012dac565
Semo <semo18@yandex.ru>
2024-12-24 09:06:23 +0000

    Временно3

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -1,14 +1,14 @@
 # flake8: noqa
 import os
 from pathlib import Path
-
 from dotenv import load_dotenv
+from django.core.management.utils import get_random_secret_key
 
 load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'fallback_secret_key_123')
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', get_random_secret_key())
 
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
